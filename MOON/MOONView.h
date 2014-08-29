4004d1561de3fbf04ab2e67e9cad84823d90ef8e
Chris Edwards <im.christopher.edwards@gmail.com>
2014-08-28 21:46:57 -0600

    Initial Commit

---
+++ MOON/MOONView.h
@@ -0,0 +1,13 @@
+//
+//  MOONView.h
+//  MOON
+//
+//  Created by Chris Edwards on 8/28/14.
+//  Copyright (c) 2014 Duck Brains. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MOONView : ScreenSaverView
+
+@end
---
+++ MOON/MOONView.m
@@ -0,0 +1,52 @@
+//
+//  MOONView.m
+//  MOON
+//
+//  Created by Chris Edwards on 8/28/14.
+//  Copyright (c) 2014 Duck Brains. All rights reserved.
+//
+
+#import "MOONView.h"
+
+@implementation MOONView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
